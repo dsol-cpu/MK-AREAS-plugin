c6f751f669ce0c610977f8edfcd412cfdde6f8b4
Coffee-Toffee <pi.tardigrade@gmail.com>
2022-03-26 21:49:11 -0400

    stuff

--- menu.py
+++ menu.py
@@ -1,7 +1,20 @@
 import bpy
 import os
 
-path = os.path.dirname(bpy.data.filepath)+"\\"
+from sys import platform
+if platform.startswith("linux"):
+    # linux
+    path = os.path.dirname(bpy.data.filepath)+"/"
+if platform.startswith("darwin"):
+    # OS X
+    path = os.path.dirname(bpy.data.filepath)+"/"
+if platform.startswith("cygwin") or platform.startswith("win32") or platform.startswith("Windows"):
+    # Windows
+    path = os.path.dirname(bpy.data.filepath)+"\\"
+else:
+    print("What are you on?")
+
+
 
 sample_string_list = []
 class KMP_Import(bpy.types.Panel):
@@ -28,9 +41,9 @@ def import_kmp(context):
     #Parse text file for all AREA objects
     file = open(path + "course.txt")
     lines = file.readlines()
-    write_kmp(lines)
-
-
+    sample_string_list = write_kmp(lines)
+    print(sample_string_list)
+    return(sample_string_list)
 
     '''
     #Make a new collection, 
@@ -44,22 +57,18 @@ def import_kmp(context):
             create_cube(current_index, lines)
         current_index+=1'''
 
-'''
-Note: I have the write_kmp function currently just write the existing kmp settings
-This is to just to make things managable for debugging / coding.
-I'll be working on the writing some sample stuff shortly hereafter, but this
-should get you an idea of what's what. (Flashbacks to hashing in data structures)
-'''
+#writes out the area into a much more readable format, return the list of list
 def write_kmp(lines) :
     hash_count = 0
     area_sector_count = 0
-#    print("writing!")
+    print("writing!")
+    temp = []
+    sample_string_list = []
     with open(path +'output.txt', 'w') as f:
         for line in lines:
             if "###############################################################################" in line:
                 hash_count += 1          # Is this quadratic time? Yes, now hush
-#                print('hash!')
-                #5th hash symbol line
+                #The area section is the 5th hash symbol line
             if hash_count == 5:
                 f.write(line)
                 if "#------------------------------------------------------------------------------" in line:
@@ -68,7 +77,14 @@ def write_kmp(lines) :
                     #2nd hash symbol with hyphens line
                 if area_sector_count >= 2 and "#------------------------------------------------------------------------------" not in line:
                     #Grab elements from current line that are not >/spaces/empty/'/n'
-                    sample_string_list = line.split()
+                    splitted_line = line.split()
+                    temp.append(','.join(splitted_line))
+                    if len(temp) == 3 :
+                        sample_string_list.append(temp)
+                        temp = []
+                    f.write(','.join(splitted_line)+'\n')
+        print(sample_string_list)
+        return(sample_string_list)
 
     #Now we want to parse from the AREA section and use each "#------" section
     #and then trim the > symbol and split the output into items
@@ -76,31 +92,25 @@ def write_kmp(lines) :
 class AREA_Cube(bpy.types.Operator):
     bl_idname = "myops.add_area_cube"
     bl_label = "Add AREA cubes"
-    
     def execute(self, context):
-        import_kmp(context)
-        create_cube(self, context)
+        sample_string_list = []
+        print(sample_string_list)
+        sample_string_list = import_kmp(context)
+        create_cube(self, context, sample_string_list)
         return {'FINISHED'}
 
-def create_cube(self, context):
+def create_cube(self, context, sample_string_list):
     print("work dammit")
     counter = 0
     current_index = 0
     position = []
     rotation = []
     scale = []
+    print(sample_string_list)
     if not sample_string_list:
         return []
     else:
-        for line in sample_string_list:
-            if counter == 0: 
-                position = [float(x) for x  in sample_string_list[3:5]] 
-            elif counter == 1:
-                rotation = [float(x) for x  in sample_string_list[3:5]]
-            elif counter == 2:
-                scale = [float(x) for x  in sample_string_list[1:3]] 
-            elif counter == 3:
-                counter = -1
+        pass #work here
                 
                 bpy.ops.primitive.primitive_cube_add(2,False,'World',position,scale)
     #            bpy.context.active_object.rotation_mode = 'XYZ'
